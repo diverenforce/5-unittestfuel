8f4c7ee3775d3130de85aea33ebea68564e1f085
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-11 21:45:16 +0630

    Establish convert return percentage

--- fuel.py
+++ fuel.py
@@ -1,9 +1,10 @@
 def main():
-    ...
+    theFraction = input('Fraction: ').strip()
 
 
 def convert(fraction):
-    ...
+    numerator, denominator = fraction.split('/')
+    return (numerator/denominator) * 100
 
 
 def gauge(percentage):
