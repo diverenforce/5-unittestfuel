67d0c18092fa194d1271417a7057a0e103ca8c53
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-11 22:41:59 +0630

    Test implement divide by zero

--- test_fuel.py
+++ test_fuel.py
@@ -5,3 +5,8 @@ def test_convert_single_value():
     assert fuel.convert('3') == None
     assert fuel.convert('4.4') == None
     assert fuel.convert('a') == None
+
+def test_convert_zero():
+    assert fuel.convert('4/0') == None
+    assert fuel.convert('1/0') == None
+    assert fuel.convert('0/0') == None
