5f2050db60d94c97a28222e8f72515cfbdd8aee6
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-11 22:43:45 +0630

    Test implement convert value

--- test_fuel.py
+++ test_fuel.py
@@ -10,3 +10,8 @@ def test_convert_zero():
     assert fuel.convert('4/0') == None
     assert fuel.convert('1/0') == None
     assert fuel.convert('0/0') == None
+
+def test_convert_value():
+    assert fuel.convert('yes/no') == None
+    assert fuel.convert('please') == None
+    assert fuel.convert('lovethewind') == None
