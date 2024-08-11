69705069b993c58a399b584ed294a66e4afcee1e
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-11 22:39:04 +0630

    Implement test single value

--- test_fuel.py
+++ test_fuel.py
@@ -1,5 +1,7 @@
 import pytest
 import fuel
 
-def test_convert():
-    fuel.convert('3/4') == 75
+def test_convert_single_value():
+    assert fuel.convert('3') == None
+    assert fuel.convert('4.4') == None
+    assert fuel.convert('a') == None
